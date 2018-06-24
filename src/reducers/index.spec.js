7145f9e3a89337c0f87a53eae28641d144e745b4
Edward Lau <edwardglau@gmail.com>
2018-06-24 15:14:57 -0700

    added logic for updating and calculating input array

--- src/reducers/index.spec.js
+++ src/reducers/index.spec.js
@@ -1,25 +1,36 @@
 import input from './input';
-
+/*
+// Initialize display
 describe('Display Reducer', () => {
   it('should handle initial state', () => {
     expect(input(undefined, {})).toEqual([]);
   })
 });
 
+// Add to display
 describe('Display Reducer', () => {
-  const payload = {
-    type: 'UPDATE_DISPLAY',
-    value: 1
-  };
+  let payload = { type: 'UPDATE_DISPLAY', value: 1 };
 
   it('should handle UPDATE_DISPLAY for initial state', () => {
-    expect(input(undefined, payload)).toEqual([1]);
+    expect(input([], { type: 'UPDATE_DISPLAY', value: 1 })).toEqual([1]);
+    expect(input([], { type: 'UPDATE_DISPLAY', value: '+' })).toEqual([]);
   });
-  it('should handle UPDATE_DISPLAY for existing state', () => {
-    expect(input([1], payload)).toEqual([1, 1]);
+  it('should handle UPDATE_DISPLAY to append numbers together', () => {
+    expect(input([1], { type: 'UPDATE_DISPLAY', value: 2 })).toEqual([12]);
+    expect(input([0], { type: 'UPDATE_DISPLAY', value: 3 })).toEqual([3]);
+    expect(input([1, '+', 2], { type: 'UPDATE_DISPLAY', value: 3 })).toEqual([1, '+', 23]);
+  });
+  it('should handle UPDATE_DISPLAY to push operators and numbers separately', () => {
+    expect(input([1], { type: 'UPDATE_DISPLAY', value: '+' })).toEqual([1, '+']);
+    expect(input([1, '+'], { type: 'UPDATE_DISPLAY', value: 2 })).toEqual([1, '+', 2]);
+  });
+  it('should handle UPDATE_DISPLAY to prevent two operators in a row', () => {
+    expect(input(['+'], { type: 'UPDATE_DISPLAY', value: '-' })).toEqual(['+']);
+    expect(input(['X'], { type: 'UPDATE_DISPLAY', value: '/' })).toEqual(['X']);
   });
 });
 
+// Clear display
 describe('Display Reducer', () => {
   const payload = {
     type: 'UPDATE_DISPLAY',
@@ -30,5 +41,32 @@ describe('Display Reducer', () => {
   });
   it('should handle UPDATE_DISPLAY to clear the display on initial state', () => {
     expect(input(undefined, payload)).toEqual([]);
+  });
+});
+*/
+// Calculate display
+describe('Display Reducer', () => {
+  const payload = {
+    type: 'UPDATE_DISPLAY',
+    value: '='
+  };
+  it('should handle UPDATE_DISPLAY to perform addition', () => {
+    expect(input([1, '+', 2], payload)).toEqual([3]);
+    expect(input([1, '+', 2, '+', 3], payload)).toEqual([6]);
+    expect(input([1, '+', 0, '+', 3], payload)).toEqual([4]);
+  });
+  it('should handle UPDATE_DISPLAY to perform subtraction', () => {
+    expect(input([2, '-', 1], payload)).toEqual([1]);
+    expect(input([1, '-', 2, '-', 3], payload)).toEqual([-4]);
+    expect(input([0, '-', 1], payload)).toEqual([-1]);
+  });
+  it('should handle UPDATE_DISPLAY to perform multiplication', () => {
+    expect(input([2, 'X', 1], payload)).toEqual([2]);
+    expect(input([1, 'X', 2, 'X', 3], payload)).toEqual([6]);
+    expect(input([0, 'X', 1], payload)).toEqual([0]);
+  });
+  it('should handle UPDATE_DISPLAY to perform division', () => {
+    expect(input([2, '/', 1], payload)).toEqual([2]);
+    expect(input([0, '/', 1], payload)).toEqual([0]);
   });
 });
--- src/reducers/input.js
+++ src/reducers/input.js
@@ -1,10 +1,55 @@
+const calculateResult = (arr) => {
+  // Balanced equation must have an odd number of variables
+  if (arr.length % 2 === 0) {
+    return input;
+  }
+  let result = arr.shift();
+  while (arr.length > 0) {
+    let operator = arr.shift();
+    let rhs = arr.shift();
+
+    if (operator === '+') {
+      result += rhs;
+    } else if (operator === '-') {
+      result -= rhs;
+    } else if (operator === 'X') {
+      result *= rhs;
+    } else {
+      result /= rhs;
+    }
+  }
+  return [result];
+}
+
+const updateInput = (input, value) => {
+  // Do not push value if it is an operator and there is no LHS number
+  if (input.length === 0 && isNaN(value)) {
+    return input;
+  }
+
+  const tail = input[input.length - 1];
+  if (!isNaN(tail) && !isNaN(value)) {
+    // Append two numbers
+    input[input.length - 1] = parseInt('' + tail + value);
+    return input;
+  } else if (isNaN(tail) && isNaN(value)) {
+    // Prevent two operators next to each other
+    return input;
+  }
+  // Push value to array
+  return [...input, value];
+}
+
 const input = (state = [], action) => {
   switch (action.type) {
     case 'UPDATE_DISPLAY':
-      if (action.value !== 'CLEAR') {
-        return [...state, action.value];
-      } else {
-        return [];
+      switch (action.value) {
+        case 'CLEAR':
+          return [];
+        case '=':
+          return calculateResult(state);
+        default:
+          return updateInput(state, action.value);
       }
     default:
       return state;
